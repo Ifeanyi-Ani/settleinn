dc5ff096d1f0ca36d2ce2071ea515b3ac737d5c0
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-11-05 12:12:29 +0100

    sync

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { Button } from "./components/ui/button";
+import { About } from "./features/about/about";
+import { AppFooter } from "./features/app-footer/app-footer";
+import { AppTopBar } from "./features/app-top-bar/app-top-bar";
+import { Contact } from "./features/contact/contact";
+import { Gallery } from "./features/gallery/gallery";
+import { HeroSlider } from "./features/hero-slider/hero-slider";
+
+function App() {
+  return (
+    <div className="w-full relative">
+      <AppTopBar />
+      <HeroSlider />
+      <main className="container mx-auto space-y-8">
+        <Gallery />
+        <About />
+        <Contact />
+      </main>
+      <AppFooter />
+
+      <Button size="lg" className="fixed bottom-4 right-4">
+        Book Now
+      </Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constant/index.ts
@@ -0,0 +1,29 @@
+import House1 from "@/assets/img9.jpg";
+import House2 from "@/assets/img10.jpg";
+import House3 from "@/assets/img11.jpg";
+
+export const sliderItem = [
+  {
+    image: House1,
+    title: "4 Bedroom flat",
+    description: "this is a 4 bedroom flat",
+  },
+  {
+    image: House2,
+    title: "4 Bedroom flat",
+    description: "this is a 4 bedroom flat",
+  },
+
+  {
+    image: House3,
+    title: "4 Bedroom flat",
+    description: "this is a 4 bedroom flat",
+  },
+];
+
+export const navLinks = [
+  { href: "#home", label: "Home" },
+  { href: "#gallery", label: "Gallery" },
+  { href: "#about", label: "About" },
+  { href: "#contact", label: "Contact" },
+];
